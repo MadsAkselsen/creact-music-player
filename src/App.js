f184cd16e934ccee2a3450b630a442b2d3e3d8ee
Mads Akselsen <akselsenmads@gmail.com>
2021-03-27 13:49:55 +0100

    added state and some styling

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Player from './components/player/Player';
 import Song from './components/song/Song';
 
 import './app.scss';
 
+import data from './data';
+
 function App() {
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setcurrentSong] = useState(songs[0]);
   return (
     <div className="App">
-      <Song />
-      <Player />
+      <Song currentSong={currentSong} />
+      <Player currentSong={currentSong} />
     </div>
   );
 }
